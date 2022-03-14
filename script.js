ec0dd3f3a80853e39d010fc7fa22b2b1ca2f25fe
fikertemariam <57527023+fikertemariam@users.noreply.github.com>
2022-03-14 05:48:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 async function fetchData() {
-    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
+   
     const record=await res.json();
-	
-	//var newd = new Intl.DateTimeFormat('am', {calendar: 'ethiopic', day: 'numeric', month: 'long',weekday: 'long',year : 'numeric', hour: 'numeric', minute : 'numeric', second: 'numeric'}).format(Date.now());
-// var n = new Intl.DateTimeFormat('am',{calendar: 'ethiopic', hour : '2-digit',minute : '2-digit', second : '2-digit'}).format(Date.now()); /
-//var newd = new Intl.DateTimeFormat('ethioaa', {calendar: 'ethiopic', day: 'numeric', month: 'long',weekday: 'long',year : 'numeric'}).format(Date.now());
 
 	var neww = new Intl.DateTimeFormat('am', {calendar: 'ethiopic', weekday: 'long'}).format(Date.now());
 	var newd = new Intl.DateTimeFormat('am', {calendar: 'ethiopic', day: 'numeric', month: 'long',year : 'numeric'}).format(Date.now());
@@ -12,9 +8,7 @@ async function fetchData() {
 
 
 	document.getElementById("weekday").innerHTML=neww;
-  //  document.getElementById("date").innerHTML=record.data[0].date;
     document.getElementById("date").innerHTML=newd;
     document.getElementById("time").innerHTML=newt;
-   // document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
 }
 fetchData();
