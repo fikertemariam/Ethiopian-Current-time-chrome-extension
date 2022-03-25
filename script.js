e9dc05b4ae59039d79b0e7a7415891e966ba88d5
fikertemariam <57527023+fikertemariam@users.noreply.github.com>
2022-03-25 14:25:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 async function fetchData() {
    
-    const record=await res.json();
-
 	var neww = new Intl.DateTimeFormat('am', {calendar: 'ethiopic', weekday: 'long'}).format(Date.now());
 	var newd = new Intl.DateTimeFormat('am', {calendar: 'ethiopic', day: 'numeric', month: 'long',year : 'numeric'}).format(Date.now());
 	var newt = new Intl.DateTimeFormat('am', {calendar: 'ethiopic', hour: 'numeric', minute : 'numeric', second: 'numeric'}).format(Date.now());
